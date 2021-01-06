1aefa96d9165b5c8c96f991c22f41f219ad1cce8
kerosin <kerosin.osm@gmail.com>
2021-01-06 12:52:42 +0100

    - using stencil web components on Angular app

--- apps/my-app/src/app/app.module.ts
+++ apps/my-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {WebComponentsAngularModule} from "@my-workspace/web-components-angular";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, WebComponentsAngularModule],
   providers: [],
   bootstrap: [AppComponent],
 })
