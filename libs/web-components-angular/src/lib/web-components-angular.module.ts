f029f233d924711d8d0845522bb0f0da0730bd9f
kerosin <kerosin.osm@gmail.com>
2021-01-06 12:54:13 +0100

    - updated web-components-angular.module

--- libs/web-components-angular/src/lib/web-components-angular.module.ts
+++ libs/web-components-angular/src/lib/web-components-angular.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { defineCustomElements } from '@my-workspace/web-components/loader';
+import {defineCustomElements} from "../../../../dist/libs/web-components/loader";
+import {MyComponent} from "../../../../dist/libs/web-components/......libsweb-components-angular/src/generated/directives/proxies";
+// import { defineCustomElements } from '@my-workspace/web-components/loader';
 
 defineCustomElements(window);
 
 @NgModule({
   imports: [CommonModule],
+  exports: [
+    MyComponent
+  ],
+  declarations: [
+    MyComponent
+  ]
 })
 export class WebComponentsAngularModule {}
